3386abca8509e637ea74517c9bb45dfb242ff7be
Pim Hopmans <pim.hopmans@live.nl>
2025-03-03 21:10:29 +0100

    verplaats 2022 archief naar public folder

--- aftrecki/next.config.ts
+++ aftrecki/next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   rewrites: async () => {
     return [
       {
+        source: "/2022",
+        destination: "/2022/index.html",
+      },
+      {
         source: "/2024",
         destination: "/2024/index.html",
       },
