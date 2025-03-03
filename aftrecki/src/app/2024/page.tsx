594ad8724e609ec8d2b1506ad77dcb8273e4eedd
tenbamyst <tjeu_mummie@hotmail.com>
2025-03-03 20:21:54 +0100

    HET HUIS

--- aftrecki/src/app/2024/page.tsx
+++ aftrecki/src/app/2024/page.tsx
@@ -1,6 +1,16 @@
+import './page.css'
+import Image from "next/image";
 
 const Page = () => {
-    return (<div>2024</div>)
+    return <div className="screen">
+        <Image
+            width={10000}
+            height={10000}
+            style={{width:'100%',height:'100%'}}
+            src={'/hetHuis.png'}
+            alt='suk mij nuts'
+        />
+    </div>
 }
 
 export default Page;
