e62bd580ed95f862763139c93ff1acf661967935
tenbamyst <tjeu_mummie@hotmail.com>
2025-03-03 21:21:38 +0100

    build?

--- aftrecki/next.config.ts
+++ aftrecki/next.config.ts
@@ -1,18 +1,19 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  rewrites: async () => {
-    return [
-      {
-        source: "/2022",
-        destination: "/2022/index.html",
-      },
-      {
-        source: "/2024",
-        destination: "/2024/index.html",
-      },
-    ];
-  },
+    output: 'export',
+    rewrites: async () => {
+        return [
+            {
+                source: "/2022",
+                destination: "/2022/index.html",
+            },
+            {
+                source: "/2024",
+                destination: "/2024/index.html",
+            },
+        ];
+    },
 };
 
 export default nextConfig;
