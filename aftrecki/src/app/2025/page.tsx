74b19dcf225ce37609baabceeb18259cede2c32c
tenbamyst <tjeu_mummie@hotmail.com>
2025-03-10 19:28:35 +0100

    try another new redirect

--- aftrecki/src/app/2025/page.tsx
+++ aftrecki/src/app/2025/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
 
-const Page = () => {
-    return <div><Image
-        width={800}
-        height={800}
+import Image from "next/image";
+import {FunctionComponent} from "react";
+
+const Page: FunctionComponent = () => {
+    const ratio = 1200 / 657
+
+    return <div style={{display: 'grid', }}><Image
+        style={{height:'100vh', width: `${ratio * 100}vh`}}
+        width={1200}
+        height={657}
         src={'/hetHuis.png'}
         alt="HetHuis"
     /></div>
--- aftrecki/src/app/page.tsx
+++ aftrecki/src/app/page.tsx
@@ -1,16 +1,16 @@
 'use client'
 
+import Page from "@/app/2025/page";
 import {useEffect} from "react";
-import {redirect} from "next/navigation";
 
 export default function Home() {
 
   useEffect(() => {
-    redirect("/2025");
+    console.log("Home page");
   })
 
   return (
-      <></>
+      <Page></Page>
     // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
     //   <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
     //     <Image
