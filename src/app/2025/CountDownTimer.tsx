356b70ca1de7f90e0bd1e7e9caf619a4481d77c3
Pim Hopmans <pim.hopmans@live.nl>
2025-03-10 21:40:50 +0100

    fix github action maybe?

--- src/app/2025/CountDownTimer.tsx
+++ src/app/2025/CountDownTimer.tsx
@@ -1,70 +1,86 @@
-'use client';
-import {FunctionComponent, useEffect, useState} from "react";
-import moment, {Moment} from "moment";
+import { FunctionComponent, useEffect, useState } from "react";
+import moment, { Moment } from "moment";
 
 type Props = {
-    timeToCountdown: Moment;
-}
+  timeToCountdown: Moment;
+};
 
-export const CountDownTimer: FunctionComponent<Props> = ({timeToCountdown}) => {
+export const CountDownTimer: FunctionComponent<Props> = ({
+  timeToCountdown,
+}) => {
+  const [now, setNow] = useState<Moment>(moment());
 
-    const [now, setNow] = useState<Moment>(moment())
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setNow(moment());
+    }, 1000);
 
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setNow(moment())
-        }, 1)
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
-        return () => {
-            clearInterval(timer)
-        }
-    }, [])
+  const duration = moment.duration(timeToCountdown.diff(now));
 
-    return <div style={{
+  return (
+    <div
+      style={{
         display: "grid",
         gridTemplateColumns: "repeat(5, 1fr)",
         justifyItems: "center",
         gridGap: "16px",
         color: "white",
-
-    }}>
-        <NumberOfCounter numberToShow={moment.duration(timeToCountdown.diff(now)).get("month")}/>
-        <NumberOfCounter numberToShow={moment.duration(timeToCountdown.diff(now)).get("day")}/>
-        <NumberOfCounter numberToShow={moment.duration(timeToCountdown.diff(now)).get("hour")}/>
-        <NumberOfCounter numberToShow={moment.duration(timeToCountdown.diff(now)).get("minute")}/>
-        <NumberOfCounter numberToShow={moment.duration(timeToCountdown.diff(now)).get("second")}/>
+      }}
+    >
+      <NumberOfCounter numberToShow={duration.months()} />
+      <NumberOfCounter numberToShow={duration.days()} />
+      <NumberOfCounter numberToShow={duration.hours()} />
+      <NumberOfCounter numberToShow={duration.minutes()} />
+      <NumberOfCounter numberToShow={duration.seconds()} />
     </div>
-}
+  );
+};
 
-export const NumberOfCounter: FunctionComponent<{ numberToShow: number }> = ({numberToShow}) => {
+export const NumberOfCounter: FunctionComponent<{ numberToShow: number }> = ({
+  numberToShow,
+}) => {
+  const [numberArray, setNumberArray] = useState<string[]>([]);
 
-    const [numberArray, setNumberArray] = useState<string[]>([])
+  useEffect(() => {
+    setNumberArray(numberToShow.toString(10).split(""));
+  }, [numberToShow]);
 
-    useEffect(() => {
-        setNumberArray(numberToShow.toString(10).split(""))
-    }, [numberToShow]);
-
-    return numberArray.map(number => {
-        return <div style={{
-            display: "grid",
-            gridTemplateColumns: "1fr",
-            gridTemplateRows: "1fr 1fr"
-        }}>
-            <div style={{
-                gridColumn: "1 / 2",
-                gridRow: "1 /2",
-                borderBottom: "1px solid lightgrey",
-                height: "100%",
-                width: "100%",
-                zIndex: 2
-            }}/>
-            <div style={{
-                gridColumn: "1 / 2",
-                gridRow: "1 /3",
-                border: "1px solid lightgrey",
-                padding: "16px",
-                backgroundColor: "rgba(00, 00, 00, 0.8)"
-            }}>{number}</div>
+  return numberArray.map((number) => {
+    return (
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr",
+          gridTemplateRows: "1fr 1fr",
+        }}
+      >
+        <div
+          style={{
+            gridColumn: "1 / 2",
+            gridRow: "1 /2",
+            borderBottom: "1px solid lightgrey",
+            height: "100%",
+            width: "100%",
+            zIndex: 2,
+          }}
+        />
+        <div
+          style={{
+            gridColumn: "1 / 2",
+            gridRow: "1 /3",
+            border: "1px solid lightgrey",
+            padding: "16px",
+            backgroundColor: "rgba(00, 00, 00, 0.8)",
+          }}
+        >
+          {number}
         </div>
-    })
-}
+      </div>
+    );
+  });
+};
