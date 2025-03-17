9d47b289207f189d1532db52037d2dc047121a68
tenbamyst <tjeu_mummie@hotmail.com>
2025-03-17 22:07:16 +0100

    dynamisch voor de haters4

--- src/app/2025/LocationBlock.tsx
+++ src/app/2025/LocationBlock.tsx
@@ -19,6 +19,7 @@ export const LocationBlock: FunctionComponent<PropsWithChildren<Props>> = ({
             setSmallscreen(window.innerWidth < widthBorder)
         });
 
+        setSmallscreen(window.innerWidth < widthBorder)
     })
 
     return (
